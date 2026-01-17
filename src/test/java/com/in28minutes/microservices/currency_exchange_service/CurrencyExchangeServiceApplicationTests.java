e93528530ea5ff836b697b1216b3715a8e4ca642
Mohammad Ibrahim Shaik <sk.mohammed5811@gmail.com>
2026-01-17 22:15:15 +0530

    h2 database setup completed

--- src/test/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.in28minutes.microservices.currency_exchange_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CurrencyExchangeServiceApplicationTests {
 
